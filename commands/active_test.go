b631f93d7ad358e46c3cf85ff86838bd5775ac4c
Rajiv M Ranganath <rajiv.ranganath@atihita.com>
2017-06-23 13:54:24 +0000

    commands/active: Remove swarm support
    * commands/active.go
    
    - Remove `item.ActiveSwarm`
    
    * commands/active_test.go
    
    - Remove `ActiveSwarm` and func `TestCmdActiveSwarm`

--- commands/active_test.go
+++ commands/active_test.go
@@ -10,22 +10,19 @@ import (
 func TestCmdActiveNone(t *testing.T) {
 	hostListItems := []HostListItem{
 		{
-			Name:        "host1",
-			ActiveHost:  false,
-			ActiveSwarm: false,
-			State:       state.Running,
+			Name:       "host1",
+			ActiveHost: false,
+			State:      state.Running,
 		},
 		{
-			Name:        "host2",
-			ActiveHost:  false,
-			ActiveSwarm: false,
-			State:       state.Running,
+			Name:       "host2",
+			ActiveHost: false,
+			State:      state.Running,
 		},
 		{
-			Name:        "host3",
-			ActiveHost:  false,
-			ActiveSwarm: false,
-			State:       state.Running,
+			Name:       "host3",
+			ActiveHost: false,
+			State:      state.Running,
 		},
 	}
 	_, err := activeHost(hostListItems)
@@ -35,22 +32,19 @@ func TestCmdActiveNone(t *testing.T) {
 func TestCmdActiveHost(t *testing.T) {
 	hostListItems := []HostListItem{
 		{
-			Name:        "host1",
-			ActiveHost:  false,
-			ActiveSwarm: false,
-			State:       state.Timeout,
+			Name:       "host1",
+			ActiveHost: false,
+			State:      state.Timeout,
 		},
 		{
-			Name:        "host2",
-			ActiveHost:  true,
-			ActiveSwarm: false,
-			State:       state.Running,
+			Name:       "host2",
+			ActiveHost: true,
+			State:      state.Running,
 		},
 		{
-			Name:        "host3",
-			ActiveHost:  false,
-			ActiveSwarm: false,
-			State:       state.Running,
+			Name:       "host3",
+			ActiveHost: false,
+			State:      state.Running,
 		},
 	}
 	active, err := activeHost(hostListItems)
@@ -58,51 +52,22 @@ func TestCmdActiveHost(t *testing.T) {
 	assert.Equal(t, active.Name, "host2")
 }
 
-func TestCmdActiveSwarm(t *testing.T) {
-	hostListItems := []HostListItem{
-		{
-			Name:        "host1",
-			ActiveHost:  false,
-			ActiveSwarm: false,
-			State:       state.Running,
-		},
-		{
-			Name:        "host2",
-			ActiveHost:  false,
-			ActiveSwarm: false,
-			State:       state.Running,
-		},
-		{
-			Name:        "host3",
-			ActiveHost:  false,
-			ActiveSwarm: true,
-			State:       state.Running,
-		},
-	}
-	active, err := activeHost(hostListItems)
-	assert.Equal(t, err, nil)
-	assert.Equal(t, active.Name, "host3")
-}
-
 func TestCmdActiveTimeout(t *testing.T) {
 	hostListItems := []HostListItem{
 		{
-			Name:        "host1",
-			ActiveHost:  false,
-			ActiveSwarm: false,
-			State:       state.Running,
+			Name:       "host1",
+			ActiveHost: false,
+			State:      state.Running,
 		},
 		{
-			Name:        "host2",
-			ActiveHost:  false,
-			ActiveSwarm: false,
-			State:       state.Running,
+			Name:       "host2",
+			ActiveHost: false,
+			State:      state.Running,
 		},
 		{
-			Name:        "host3",
-			ActiveHost:  false,
-			ActiveSwarm: false,
-			State:       state.Timeout,
+			Name:       "host3",
+			ActiveHost: false,
+			State:      state.Timeout,
 		},
 	}
 	_, err := activeHost(hostListItems)
