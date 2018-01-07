f4f60cb614e1cfe0f1db0fcec183918cd040de54
Rajiv M Ranganath <rajiv.ranganath@atihita.com>
2018-01-07 04:33:25 +0000

    commands/restart: Rename to `lambda-machine-local`

--- commands/restart.go
+++ commands/restart.go
@@ -10,7 +10,7 @@ func cmdRestart(c CommandLine, api libmachine.API) error {
 		return err
 	}
 
-	log.Info("Restarted machines may have new IP addresses. You may need to re-run the `docker-machine env` command.")
+	log.Info("Restarted machines may have new IP addresses. You may need to re-run the `lambda-machine-local env` command.")
 
 	return nil
 }
