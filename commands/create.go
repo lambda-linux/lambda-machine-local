7e6391613e7b3ef92300539804a5e2336f59629d
Rajiv M Ranganath <rajiv.ranganath@atihita.com>
2018-01-07 04:33:23 +0000

    commands/create: Use `../lambda-machine-local/commands/mcndirs`

--- commands/create.go
+++ commands/create.go
@@ -15,7 +15,6 @@ import (
 	"time"
 
 	"github.com/codegangsta/cli"
-	"github.com/docker/machine/commands/mcndirs"
 	"github.com/docker/machine/libmachine"
 	"github.com/docker/machine/libmachine/auth"
 	"github.com/docker/machine/libmachine/crashreport"
@@ -27,6 +26,7 @@ import (
 	"github.com/docker/machine/libmachine/mcnerror"
 	"github.com/docker/machine/libmachine/mcnflag"
 	"github.com/docker/machine/libmachine/swarm"
+	"github.com/lambda-linux/lambda-machine-local/commands/mcndirs"
 )
 
 var (
