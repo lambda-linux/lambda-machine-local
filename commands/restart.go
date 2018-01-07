b4a0f51f8cbbb453164a504f9564995d36d8c765
Rajiv M Ranganath <rajiv.ranganath@atihita.com>
2018-01-07 04:33:25 +0000

    commands/restart: Remove log message

--- commands/restart.go
+++ commands/restart.go
@@ -2,15 +2,12 @@ package commands
 
 import (
 	"github.com/docker/machine/libmachine"
-	"github.com/docker/machine/libmachine/log"
 )
 
 func cmdRestart(c CommandLine, api libmachine.API) error {
 	if err := runAction("restart", c, api); err != nil {
 		return err
 	}
-
-	log.Info("Restarted machines may have new IP addresses. You may need to re-run the `lambda-machine-local env` command.")
 
 	return nil
 }
