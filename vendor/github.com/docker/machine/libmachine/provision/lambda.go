901a9426efcdf0e4c779d985e3240f6a232004aa
Rajiv M Ranganath <rajiv.ranganath@atihita.com>
2018-01-10 15:23:11 +0000

    libmachine/provision: Lambda2 changes

--- vendor/github.com/docker/machine/libmachine/provision/lambda.go
+++ vendor/github.com/docker/machine/libmachine/provision/lambda.go
@@ -1,19 +1,18 @@
 package provision
 
 import (
-	"fmt"
-	"io/ioutil"
-	"path/filepath"
+	"encoding/json"
 
 	"github.com/docker/machine/libmachine/auth"
-	"github.com/docker/machine/libmachine/cert"
 	"github.com/docker/machine/libmachine/drivers"
 	"github.com/docker/machine/libmachine/engine"
 	"github.com/docker/machine/libmachine/log"
 	"github.com/docker/machine/libmachine/mcnutils"
 	"github.com/docker/machine/libmachine/provision/pkgaction"
 	"github.com/docker/machine/libmachine/provision/serviceaction"
+	"github.com/docker/machine/libmachine/state"
 	"github.com/docker/machine/libmachine/swarm"
+	"github.com/lambda-linux/lambda-machine-local/commands/mcndirs"
 )
 
 func init() {
@@ -40,8 +39,7 @@ func (provisioner *LambdaProvisioner) String() string {
 }
 
 func (provisioner *LambdaProvisioner) Service(name string, action serviceaction.ServiceAction) error {
-	_, err := provisioner.SSHCommand(fmt.Sprintf("sudo service %s %s", name, action.String()))
-	return err
+	return nil
 }
 
 func (provisioner *LambdaProvisioner) CompatibleWithHost() bool {
@@ -57,7 +55,12 @@ func (provisioner *LambdaProvisioner) GetOsReleaseInfo() (*OsRelease, error) {
 }
 
 func (provisioner *LambdaProvisioner) Package(name string, action pkgaction.PackageAction) error {
-	return fmt.Errorf("Not supported in Lambda Linux Provisioner")
+	if name == "docker" && action == pkgaction.Upgrade {
+		if err := provisioner.upgradeIso(); err != nil {
+			return err
+		}
+	}
+	return nil
 }
 
 func (provisioner *LambdaProvisioner) Hostname() (string, error) {
@@ -66,7 +69,7 @@ func (provisioner *LambdaProvisioner) Hostname() (string, error) {
 
 func (provisioner *LambdaProvisioner) SetHostname(hostname string) error {
 	// We do not set hostname here. The hostname is set using
-	// `/etc/dhcp/dhclient.d/sethostname.sh`
+	// `/etc/udhcpc/post-bound/set-hostname`
 	return nil
 }
 
@@ -91,42 +94,7 @@ func (provisioner *LambdaProvisioner) GenerateDockerOptions(dockerPort int) (*Do
 }
 
 func (provisioner *LambdaProvisioner) Provision(swarmOptions swarm.Options, authOptions auth.Options, engineOptions engine.Options) error {
-	var (
-		err error
-	)
-
-	provisioner.AuthOptions = authOptions
-	provisioner.EngineOptions = engineOptions
-
-	// stop docker if it is running
-	if err := provisioner.Service("docker", serviceaction.Stop); err != nil {
-		return err
-	}
-	// restart lvm2-monitor as a good measure
-	if err := provisioner.Service("lvm2-monitor", serviceaction.Restart); err != nil {
-		return err
-	}
-
-	// We don't use `makeDockerOptionsDir` as that is taken care of by
-	// imagebuilder
-
-	provisioner.AuthOptions = setRemoteAuthOptions(provisioner)
-
-	// NOTE: we do not use `ConfigureAuth`. Instead we use `lambdaConfigureAuth`
-	if err = lambdaConfigureAuth(provisioner); err != nil {
-		return err
-	}
-
-	// restart lvm2-monitor as a good measure before starting docker
-	if err := provisioner.Service("lvm2-monitor", serviceaction.Restart); err != nil {
-		return err
-	}
-	// start docker
-	if err := provisioner.Service("docker", serviceaction.Start); err != nil {
-		return err
-	}
-
-	return WaitForDocker(provisioner, engine.DefaultPort)
+	return nil
 }
 
 func (provisioner *LambdaProvisioner) SSHCommand(args string) (string, error) {
@@ -137,103 +105,47 @@ func (provisioner *LambdaProvisioner) GetDriver() drivers.Driver {
 	return provisioner.Driver
 }
 
-func lambdaConfigureAuth(p Provisioner) error {
-	var (
-		err error
-	)
+func (provisioner *LambdaProvisioner) upgradeIso() error {
+	// Copied from Boot2Docker's upgradeIso func
+	b2dutils := mcnutils.NewB2dUtils(mcndirs.GetBaseDir())
 
-	driver := p.GetDriver()
-	machineName := driver.GetMachineName()
-	authOptions := p.GetAuthOptions()
-
-	org := mcnutils.GetUsername() + "." + machineName
-	bits := 2048
-
-	ip, err := driver.GetIP()
+	// Check if the driver has specified a custom b2d url
+	jsonDriver, err := json.Marshal(provisioner.GetDriver())
 	if err != nil {
 		return err
+
+	}
+	var d struct {
+		Boot2DockerURL string
+	}
+	json.Unmarshal(jsonDriver, &d)
+
+	log.Info("Stopping machine to do the upgrade...")
+
+	if err := provisioner.Driver.Stop(); err != nil {
+		return err
+
 	}
 
-	log.Info("Copying certs to the local machine directory...")
-
-	if err := mcnutils.CopyFile(authOptions.CaCertPath, filepath.Join(authOptions.StorePath, "ca.pem")); err != nil {
-		return fmt.Errorf("Copying ca.pem to machine dir failed: %s", err)
-	}
-
-	if err := mcnutils.CopyFile(authOptions.ClientCertPath, filepath.Join(authOptions.StorePath, "cert.pem")); err != nil {
-		return fmt.Errorf("Copying cert.pem to machine dir failed: %s", err)
-	}
-
-	if err := mcnutils.CopyFile(authOptions.ClientKeyPath, filepath.Join(authOptions.StorePath, "key.pem")); err != nil {
-		return fmt.Errorf("Copying key.pem to machine dir failed: %s", err)
-	}
-
-	// The Host IP is always added to the certificate's SANs list
-	hosts := append(authOptions.ServerCertSANs, ip, "localhost")
-	log.Debugf("generating server cert: %s ca-key=%s private-key=%s org=%s san=%s",
-		authOptions.ServerCertPath,
-		authOptions.CaCertPath,
-		authOptions.CaPrivateKeyPath,
-		org,
-		hosts,
-	)
-
-	// TODO: Switch to passing just authOptions to this func
-	// instead of all these individual fields
-	err = cert.GenerateCert(&cert.Options{
-		Hosts:       hosts,
-		CertFile:    authOptions.ServerCertPath,
-		KeyFile:     authOptions.ServerKeyPath,
-		CAFile:      authOptions.CaCertPath,
-		CAKeyFile:   authOptions.CaPrivateKeyPath,
-		Org:         org,
-		Bits:        bits,
-		SwarmMaster: false,
-	})
-
-	if err != nil {
-		return fmt.Errorf("error generating server cert: %s", err)
-	}
-
-	// docker service should be stopped in our case
-
-	if _, err := p.SSHCommand(`if [ ! -z "$(ip link show docker0)" ]; then sudo ip link delete docker0; fi`); err != nil {
+	if err := mcnutils.WaitFor(drivers.MachineInState(provisioner.Driver, state.Stopped)); err != nil {
 		return err
 	}
 
-	// upload certs and configure TLS auth
-	caCert, err := ioutil.ReadFile(authOptions.CaCertPath)
-	if err != nil {
+	machineName := provisioner.GetDriver().GetMachineName()
+
+	log.Infof("Upgrading machine %q...", machineName)
+
+	// Either download the latest version of the b2d url that was explicitly
+	// specified when creating the VM or copy the (updated) default ISO
+	if err := b2dutils.CopyIsoToMachineDir(d.Boot2DockerURL, machineName); err != nil {
 		return err
 	}
 
-	serverCert, err := ioutil.ReadFile(authOptions.ServerCertPath)
-	if err != nil {
-		return err
-	}
-	serverKey, err := ioutil.ReadFile(authOptions.ServerKeyPath)
-	if err != nil {
+	log.Infof("Starting machine back up...")
+
+	if err := provisioner.Driver.Start(); err != nil {
 		return err
 	}
 
-	log.Info("Copying certs to the remote machine...")
-
-	// printf will choke if we don't pass a format string because of the
-	// dashes, so that's the reason for the '%%s'
-	certTransferCmdFmt := "printf '%%s' '%s' | sudo tee %s"
-
-	// These ones are for Jessie and Mike <3 <3 <3
-	if _, err := p.SSHCommand(fmt.Sprintf(certTransferCmdFmt, string(caCert), authOptions.CaCertRemotePath)); err != nil {
-		return err
-	}
-
-	if _, err := p.SSHCommand(fmt.Sprintf(certTransferCmdFmt, string(serverCert), authOptions.ServerCertRemotePath)); err != nil {
-		return err
-	}
-
-	if _, err := p.SSHCommand(fmt.Sprintf(certTransferCmdFmt, string(serverKey), authOptions.ServerKeyRemotePath)); err != nil {
-		return err
-	}
-
-	return nil
+	return mcnutils.WaitFor(drivers.MachineInState(provisioner.Driver, state.Running))
 }
