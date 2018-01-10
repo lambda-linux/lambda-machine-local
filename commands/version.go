fdb68eef32efd6c9f82109eba8f8b02af4c11284
Rajiv M Ranganath <rajiv.ranganath@atihita.com>
2018-01-10 15:31:40 +0000

    commands/version: Fix the number of arguments that can be passed

--- commands/version.go
+++ commands/version.go
@@ -1,13 +1,10 @@
 package commands
 
 import (
-	"fmt"
-
 	"io"
 	"os"
 
 	"github.com/docker/machine/libmachine"
-	"github.com/docker/machine/libmachine/mcndockerclient"
 )
 
 func cmdVersion(c CommandLine, api libmachine.API) error {
@@ -15,26 +12,10 @@ func cmdVersion(c CommandLine, api libmachine.API) error {
 }
 
 func printVersion(c CommandLine, api libmachine.API, out io.Writer) error {
-	if len(c.Args()) == 0 {
-		c.ShowVersion()
-		return nil
+	if len(c.Args()) != 0 {
+		return ErrTooManyArguments
 	}
 
-	if len(c.Args()) != 1 {
-		return ErrExpectedOneMachine
-	}
-
-	host, err := api.Load(c.Args().First())
-	if err != nil {
-		return err
-	}
-
-	version, err := mcndockerclient.DockerVersion(host)
-	if err != nil {
-		return err
-	}
-
-	fmt.Fprintln(out, version)
-
+	c.ShowVersion()
 	return nil
 }
